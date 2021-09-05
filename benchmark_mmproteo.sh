1e55ffcad8cc115fc6b72b987696cbe372a3e522
Mirko Krause <krause@codebase.one>
2021-09-05 13:52:01 +0200

    fix mmproteo benchmark due to missing MGF FTP files

--- benchmark_mmproteo.sh
+++ benchmark_mmproteo.sh
@@ -2,16 +2,18 @@
 
 echoerr() { echo "$@" 1>&2; }
 
-for i in $(echo -e "1\n4\n16"); do
+for i in $(echo -e "1\n2\n4\n8"); do
 	mkdir $i
 	cd $i
 	echoerr cores=$i
 	BASE_CMD="mmproteo -p PXD010000 --thread-count $i --log-to-stdout"
 	echoerr download:
-	for e in $(echo -e "mgf\nmzid\nmzml"); do
+	for e in $(echo -e "raw\nmzid\nmzml"); do
 		echo extension=$e
-		time $BASE_CMD -n 64 -e $e download
+		time $BASE_CMD -n 8 -e $e download
 	done
+	echoerr convertraw:
+	time $BASE_CMD --thermo-output-format mgf convertraw
 	echoerr extract:
 	time $BASE_CMD extract
 	echoerr mz2parquet:
