a6b3e78ab150676414618458578b4d2ac1179e22
Mirko Krause <krause@codebase.one>
2021-09-06 14:34:14 +0200

    reformat log messages in raw file processing

--- src/mmproteo/utils/formats/raw.py
+++ src/mmproteo/utils/formats/raw.py
@@ -54,7 +54,7 @@ def stop_thermo_docker_container(thermo_docker_container_name: str = Config.defa
     stop_command = utils.stop_docker_container(container_name=thermo_docker_container_name)
     logger.assert_true(condition=not utils.is_docker_container_running(thermo_docker_container_name),
                        error_msg=subject + " still seems to be running")
-    logger.info(f"Stopped {subject} using {stop_command}")
+    logger.info(f"Stopped {subject} using '{stop_command}'")
 
 
 _THERMO_RAW_FILE_PARSER_OUTPUT_FORMAT_IDS = {
@@ -79,8 +79,8 @@ def get_string_of_thermo_raw_file_parser_output_formats(format_quote: str = Conf
 def assert_valid_thermo_output_format(output_format: str, logger: log.Logger = log.DEFAULT_LOGGER) -> \
         Optional[NoReturn]:
     logger.assert_true(output_format in get_thermo_raw_file_parser_output_formats(),
-                       "Invalid output format '%s'. Currently allowed formats are: [%s]"
-                       % (output_format, get_string_of_thermo_raw_file_parser_output_formats()))
+                       f"Invalid output format '{output_format}'. Currently allowed formats are: "
+                       f"[{get_string_of_thermo_raw_file_parser_output_formats()}]")
     return None
 
 
@@ -98,18 +98,18 @@ def convert_raw_file(filename: Optional[str],
     base_filename, file_ext = separate_extension(filename=filename,
                                                  extensions={"raw"})
     if len(file_ext) == 0:
-        logger.debug("Cannot convert file '%s', unknown extension" % filename)
+        logger.debug(f"Cannot convert file '{filename}', unknown extension")
         return None
 
     converted_filename = base_filename + "." + output_format
 
     if skip_existing and os.path.isfile(converted_filename):
-        logger.info('Skipping conversion, because "%s" already exists' % converted_filename)
+        logger.info(f"Skipping conversion, because '{converted_filename}' already exists")
         return converted_filename
 
     logger.assert_true(utils.is_docker_container_running(thermo_docker_container_name),
-                       "There is no running ThermoRawFileParser Docker container with the name " +
-                       thermo_docker_container_name)
+                       "There is no running ThermoRawFileParser Docker container with the name "
+                       f"'{thermo_docker_container_name}'")
 
     output_format_id = _THERMO_RAW_FILE_PARSER_OUTPUT_FORMAT_IDS[output_format]
 
@@ -117,13 +117,13 @@ def convert_raw_file(filename: Optional[str],
                                          format=output_format_id,
                                          input=filename)
 
-    logger.debug("Converting file using command '%s'" % command)
+    logger.debug(f"Converting file using command '{command}'")
     return_code = os.system(command)
     if return_code == 0:
-        logger.info("Converted file " + filename)
+        logger.info(f"Converted file '{filename}'")
         return converted_filename
     else:
-        logger.warning('Failed converting file "%s" (return code = %d)' % (filename, return_code))
+        logger.warning(f"Failed converting file '{filename}' (return code = {return_code})")
         return None
 
 
