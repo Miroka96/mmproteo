4d239f5279a3e09496e5c88337ae5d031c7cf8f7
Mirko Krause <krause@codebase.one>
2021-09-06 15:45:07 +0200

    fix download.sh call in run_benchmark.sh

--- benchmarking/run_benchmark.sh
+++ benchmarking/run_benchmark.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-$(dirname $0)/downloaded/download.sh
-$(dirname $0)/benchmark_mmproteo.sh 3>&1 1>&2 2>&3 | tee -a benchmark.log
+BASE_PATH=$(realpath $(dirname $0))
+
+mkdir downloaded
+cd downloaded
+$BASE_PATH/download.sh
+cd ..
+$BASE_PATH/benchmark_mmproteo.sh 3>&1 1>&2 2>&3 | tee -a benchmark.log
