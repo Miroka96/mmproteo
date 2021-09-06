8a81325b42b0334bc3a28ceef521dbff209bec4b
Mirko Krause <krause@codebase.one>
2021-09-06 15:10:27 +0200

    transform benchmark into CSV output

--- benchmarking/benchmark_mmproteo.sh
+++ benchmarking/benchmark_mmproteo.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echoerr() { echo "$@" 1>&2; }
+echoerr() { echo -ne "$@" 1>&2; }
 TIME_CMD="/usr/bin/time --format=%K,%e"
 
-echoerr "cores,operation,part,averageTotalMemoryInKB,elapsedRealTime"
+echoerr "cores,operation,part,averageTotalMemoryInKB,elapsedRealTime\n"
 for i in $(echo -e "1\n2\n4\n8"); do
 	mkdir "$i"
 	cd "$i" || (echoerr "could not change into $(pwd)/$i"; exit 1)
@@ -13,16 +13,21 @@ for i in $(echo -e "1\n2\n4\n8"); do
 	echo download:
 	for e in $(echo -e "raw\nmzid\nmzml"); do
 		echo extension="$e"
-		echoerr "$i,download,$e,$($TIME_CMD $BASE_CMD -n 8 -e "$e" download 3>&1 1>&2 2>&3)"
+		echoerr "$i,download,$e,"
+		$TIME_CMD $BASE_CMD -n 8 -e "$e" download
 	done
 	echo convertraw:
-	echoerr "$i,convertraw,,$($TIME_CMD $BASE_CMD --thermo-output-format mgf convertraw 3>&1 1>&2 2>&3)"
+	echoerr "$i,convertraw,,"
+	$TIME_CMD $BASE_CMD --thermo-output-format mgf convertraw
 	echo extract:
-	echoerr "$i,extract,,$($TIME_CMD $BASE_CMD extract 3>&1 1>&2 2>&3)"
+	echoerr "$i,extract,,"
+	$TIME_CMD $BASE_CMD extract
 	echo mz2parquet:
-	echoerr "$i,mz2parquet,,$($TIME_CMD $BASE_CMD mz2parquet 3>&1 1>&2 2>&3)"
+	echoerr "$i,mz2parquet,,"
+	$TIME_CMD $BASE_CMD mz2parquet
 	echo mgf2parquet:
-	echoerr "$i,mgf2parquet,,$($TIME_CMD $BASE_CMD mgf2parquet 3>&1 1>&2 2>&3)"
+	echoerr "$i,mgf2parquet,,"
+	$TIME_CMD $BASE_CMD mgf2parquet
 	echo
 	cd ..
 done
