67537478f556771dfe60aea77377e03cd519e0bc
Mirko Krause <krause@codebase.one>
2021-09-06 14:43:40 +0200

    redirecting benchmark outputs

--- benchmarking/benchmark_mmproteo.sh
+++ benchmarking/benchmark_mmproteo.sh
@@ -13,16 +13,16 @@ for i in $(echo -e "1\n2\n4\n8"); do
 	echo download:
 	for e in $(echo -e "raw\nmzid\nmzml"); do
 		echo extension="$e"
-		echoerr "$i,download,$e,$($TIME_CMD $BASE_CMD -n 8 -e "$e" download)"
+		echoerr "$i,download,$e,$($TIME_CMD $BASE_CMD -n 8 -e "$e" download 3>&1 1>&2 2>&3)"
 	done
 	echo convertraw:
-	echoerr "$i,convertraw,,$($TIME_CMD $BASE_CMD --thermo-output-format mgf convertraw)"
+	echoerr "$i,convertraw,,$($TIME_CMD $BASE_CMD --thermo-output-format mgf convertraw 3>&1 1>&2 2>&3)"
 	echo extract:
-	echoerr "$i,extract,,$($TIME_CMD $BASE_CMD extract)"
+	echoerr "$i,extract,,$($TIME_CMD $BASE_CMD extract 3>&1 1>&2 2>&3)"
 	echo mz2parquet:
-	echoerr "$i,mz2parquet,,$($TIME_CMD $BASE_CMD mz2parquet)"
+	echoerr "$i,mz2parquet,,$($TIME_CMD $BASE_CMD mz2parquet 3>&1 1>&2 2>&3)"
 	echo mgf2parquet:
-	echoerr "$i,mgf2parquet,,$($TIME_CMD $BASE_CMD mgf2parquet)"
+	echoerr "$i,mgf2parquet,,$($TIME_CMD $BASE_CMD mgf2parquet 3>&1 1>&2 2>&3)"
 	echo
 	cd ..
 done
